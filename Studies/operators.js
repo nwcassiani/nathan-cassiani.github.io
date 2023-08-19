0fd79ff1b7aaeab9c34320f148d2098b8cf49730
Nathan Cassiani <nwcassiani@gmail.com>
2023-08-19 17:48:41 +0000

    commit operators.js file

---
+++ Studies/operators.js
@@ -0,0 +1,21 @@
+/**
+ * OPERATORS
+ * 
+ * 0. In Javascript, operators are special symbols used to perform operations on operands (values and variables). Operators 
+ * are used to assign values, perform arithmetic operations, compare values, and more. 
+ * 
+ * 1. Assignment operators are used to assign value to variables. The "=" operator is called the assignment operator; using it 
+ * assigns value to a variable.
+ * 
+ * 2. Arithmetic operators are used to perfrom arithmetic on values. Most commonly we would think of using arithmetic operators 
+ * on Number values, but we can also use the + operator to concatenate strings.
+ * 
+ * 3. Comparison operators are used in logical statements to determine equality or difference between values and variables. They  
+ * can be used in conditional statements to compare values and take action depending on the result.
+ * 
+ * 4. Logical operators are used to determine the logic between variables or values
+ * 
+ * 5. A unary operation is an operation with only one operand.
+ * 
+ * 6. The ternary operator, or conditional operator, assigns a value to a variable based on some condition
+ */
---
+++ Studies/variables.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * VARIABLES
+ * 
+ * 0: Variables are named containers for data. (5 sentences, all vital info)
+ * 
+ * 1: Declaration and assignment
+ * 
+ * 2: var, let, const
+ * 
+ * 3: 
+ */

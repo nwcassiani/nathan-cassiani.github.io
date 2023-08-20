f5d54e0e4b2a136ea1b6444570f568d485f88bf8
Nathan Cassiani <nwcassiani@gmail.com>
2023-08-20 20:25:23 +0000

    commit operators.js update

--- Studies/operators.js
+++ Studies/operators.js
@@ -13,15 +13,28 @@
  * 3. Comparison operators are used in logical statements to determine equality or difference between values and variables. They  
  * can be used in conditional statements to compare values and take action depending on the result.
  * 
- * 4. Logical operators are used to determine the logic between variables or values
+ * 4. Logical operators are used to determine the logic between variables or values, and are typically used with Boolean values.
  * 
- * 5. A unary operation is an operation with only one operand.
+ * 5. A unary operator operates on only one value.
  * 
  * 6. The ternary operator, or conditional operator, assigns a value to a variable based on some condition. This is the
  * only operator in Javascript that takes on three operands: a condition followed by a ?, then an expression to execute if the 
  * condition is truthy followed by a :, and finally the expression to execute if the condition is falsey. This operator is 
  * frequently used as an alternative to an if...else statement.
  */
+
+// 5. Unary Operators
+// the delete operator deletes a property from an object
+var myObj = {
+    name: "Nathan",
+    age: 37
+};
+delete myObj.age;
+console.log(myObj); // prints { name: "Nathan" } to console, age property has been removed
+
+// typeof operator 
+// the typeof operator returns a string indicating the type of value passed to the operator
+console.log(typeof myObj.name); // prints => string
 
 // 6. Ternary Operator
 // Syntax: condition ? exprIfTrue : exprIfFalse

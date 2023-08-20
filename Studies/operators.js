84a115a38ae005d688808ffdb8ebeda9f0a62120
Nathan Cassiani <nwcassiani@gmail.com>
2023-08-20 20:01:07 +0000

    commit operators.js update

--- Studies/operators.js
+++ Studies/operators.js
@@ -17,5 +17,18 @@
  * 
  * 5. A unary operation is an operation with only one operand.
  * 
- * 6. The ternary operator, or conditional operator, assigns a value to a variable based on some condition
+ * 6. The ternary operator, or conditional operator, assigns a value to a variable based on some condition. This is the
+ * only operator in Javascript that takes on three operands: a condition followed by a ?, then an expression to execute if the 
+ * condition is truthy followed by a :, and finally the expression to execute if the condition is falsey. This operator is 
+ * frequently used as an alternative to an if...else statement.
  */
+
+// 6. Ternary Operator
+// Syntax: condition ? exprIfTrue : exprIfFalse
+// condition: an expression whose value is used as a condition
+// exprIfTrue: an expression which is executed if the condition evaluates to a truthy value
+// exprIfFalse: an expression which is executed if the condtion evaluates to a falsey value
+var myAge = 37;
+var drink = myAge >= 21 ? "Wine" : "Water";
+console.log(drink); // prints "Wine" to console
+// Can also be chained similar to an if...else chain

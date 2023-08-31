282f43f83992eaaefd09247a32e7ece9a0166bb9
Nathan Cassiani <nwcassiani@gmail.com>
2023-08-31 18:00:58 +0000

    underpants pluck

--- projects/underpants/underpants.js
+++ projects/underpants/underpants.js
@@ -364,11 +364,11 @@ _.map = function(collection, func) {
 
 _.pluck = function(array, prop) {
 
-    // // return array containing value of prop for every element in array
-    // // use map function
-    // // loop over array
+    // return array containing value of prop for every element in array
+    // use map function, map function returns an array
+    // loop over array
     for (let i = 0; i < array.length; i++) {
-         return _.map(array[i], function() {
+        return _.map(array[i], function() {
             return array[i][prop];
          });
         }

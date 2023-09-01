df84594078c3f1bae6469bd2b7831f01d70fd157
Nathan Cassiani <nwcassiani@gmail.com>
2023-09-01 19:08:42 +0000

    studies data types

---
+++ Studies/data-types.js
@@ -0,0 +1,21 @@
+/**
+ * DATA TYPES
+ * 
+ * 0. Data types are how we categorize different types of data in Javascript. They have some unique and 
+ * shared properties between them, and allow for the Javascript interpreter to work with different kinds of
+ * data.
+ * 1. Number refers to numeric data.
+ * 2. String refers to a character data type.
+ * 3. A Boolean is a true of false value.
+ * 4. An array is a zero-indexed list stored inside brackets. It is a type of object, and is therefore a
+ * complex data type.
+ * 5. An object is a collection of key/value pairs, called properties, stored inside curly braces.
+ * 6. A function is a set of statements that performs a task or calculates a value.
+ * 7. undefined refers to a value that has no value, or not initialized.
+ * 8. The null data type refers to a value that is intentionally nullified by the programmer, and has one
+ * value: null. <null> indicates the absence of an object, as opposed to the absence of a value.
+ * 9. NaN not-a-number
+ * 10. Infinity and -Infinity
+ * 11. Difference btw primitive/simple and complex data types
+ * 12. Copy by value vs copy by reference
+ */

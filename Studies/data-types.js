23f6ed89fc5ed244132c464887b6bf3970ebda43
Nathan Cassiani <nwcassiani@gmail.com>
2023-09-01 19:29:46 +0000

    studies data types

--- Studies/data-types.js
+++ Studies/data-types.js
@@ -5,17 +5,23 @@
  * shared properties between them, and allow for the Javascript interpreter to work with different kinds of
  * data.
  * 1. Number refers to numeric data.
- * 2. String refers to a character data type.
- * 3. A Boolean is a true of false value.
- * 4. An array is a zero-indexed list stored inside brackets. It is a type of object, and is therefore a
- * complex data type.
- * 5. An object is a collection of key/value pairs, called properties, stored inside curly braces.
- * 6. A function is a set of statements that performs a task or calculates a value.
+ * 2. String refers to textual or character data. The position of a character in a string is the character's
+ * index.
+ * 3. A Boolean is a logical data type and has two values: true or false.
+ * 4. An array is a zero-indexed list of values stored inside brackets. They can store different data types.
+ * It is a type of object, and is therefore a complex data type.
+ * 5. An object is a collection of key/value pairs, called properties, stored inside curly braces. The values
+ * in an object are stored at specific keys.
+ * 6. A function is a set of statements that performs a task or calculates a value. A function must take 
+ * some sort of input and return some output, with some type of relationship between the input and output.
  * 7. undefined refers to a value that has no value, or not initialized.
  * 8. The null data type refers to a value that is intentionally nullified by the programmer, and has one
  * value: null. <null> indicates the absence of an object, as opposed to the absence of a value.
- * 9. NaN not-a-number
- * 10. Infinity and -Infinity
+ * 9. NaN refers to not-a-number. It is a special kind of number value that's typically encountered when the 
+ * result of an arithmetic operation cannot be expressed as a number. It is the only value in Javascript that
+ * is not equal to itself.
+ * 10. Infinity is a numeric value representing infinity. The value of (positive) Infinity is greater than any
+ * other number. -Infinity is the same as the negative value of infinity.
  * 11. Difference btw primitive/simple and complex data types
  * 12. Copy by value vs copy by reference
  */

eac9c6774cffa3b1d86b2f12facd289ce38771d1
Nathan Cassiani <nwcassiani@gmail.com>
2023-09-07 20:50:29 +0000

    recursion letterTally

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -349,7 +349,19 @@ var flatten = function(arrays) {
 
 // 30. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
-var letterTally = function(str, obj) {
+var letterTally = function(str, obj={}) {
+  // base 
+  if (str.length === 0){
+    return obj;
+  }
+  // recursion
+  if (obj[str[0]]){
+    obj[str[0]] += 1;
+  } else {
+    obj[str[0]] = 1;
+  }
+
+  return letterTally(str.slice(1), obj);
 };
 
 // 31. Eliminate consecutive duplicates in a list.  If the list contains repeated

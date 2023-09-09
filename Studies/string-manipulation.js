9136d8dd50ff8e83de1e27efc948cb660a68c7e4
Nathan Cassiani <nwcassiani@gmail.com>
2023-09-09 16:50:26 +0000

    studies string manipulation

--- Studies/string-manipulation.js
+++ Studies/string-manipulation.js
@@ -31,13 +31,34 @@ console.log(fullName.split(' ')); // prints => ['Nathan ', 'Cassiani']
 console.log(fullName.split('')); // prints => ['N', 'a', 't', 'h', 'a', 'n', ' ', 'C', 'a', 's', 's', 'i', 'a', 'n', 'i']
 
 //from()
+// allows us to make an array from a string value, similar to array.split('');
+let nameChars = Array.from(fullName);
+console.log(nameChars); // prints => ['N', 'a', 't', 'h', 'a', 'n', ' ', 'C', 'a', 's', 's', 'i', 'a', 'n', 'i']
 
-//spread operator
+//spread operator (...)
+// another way to make an array from a string value, using the spread operator to the left of the variable name in brackets
+let nameArray = [...fullName];
+console.log(nameArray); // prints => ['N', 'a', 't', 'h', 'a', 'n', ' ', 'C', 'a', 's', 's', 'i', 'a', 'n', 'i']
+
 
 // reverse
+/* the array method reverse() returns an array of the input array's elements reversed. This can be used to
+reverse a string by splitting the string into an array, reversing the elements, then joining the array back
+into a string
+*/
+let reversedName = fullName.split('').reverse().join('');
+console.log(reversedName); // prints => inaissaC nahtaN
 
-// toUpperCase and toLowerCase
+// toUpperCase() and toLowerCase()
+// will convert all characters to upper or lower case, respectively
+console.log(fullName.toUpperCase()); // prints => NATHAN CASSIANI
+console.log(fullName.toLowerCase()); // prints => nathan cassiani
 
-// repeat
+// repeat()
+// a string method, allows us to repeat a string a specified number of times
+console.log(fullName.repeat(2)); // prints => Nathan CassianiNathan Cassiani
 
-// replace
+// replace()
+// will replace all specified characters, between / and /g, with a given character in quotes, like so:
+console.log(fullName.replace(/a/g, " ")); // prints => N th n C ssi ni
+// has replaced all "a" characters with a blank space

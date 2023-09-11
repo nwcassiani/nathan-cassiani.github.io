d606f061cd9f10b6fd0192298061911fc244b533
Nathan Cassiani <nwcassiani@gmail.com>
2023-09-11 16:10:09 +0000

    studies loops

---
+++ Studies/loops.js
@@ -0,0 +1,12 @@
+/**
+ * LOOPS
+ * 0. Loops offer a quick and easy way to repeat an action in our code a certain number of times. The various
+ * loop mechanisms offer different ways to determine the start and end points of the loop. There are various 
+ * situations that are more easily served by one type of loop over the others.
+ * 1. Explain while, for in, and for loops. A while loop loops through a block of code as long as a specified
+ * condition is true. For loops loop over a block of code until a specified condtion evaluates to false. For
+ * in loops are designed to loop over objects. 
+ * 2. Be able to loop any number of times, forward counting up to some liimit, backward counting down to zero.
+ * 3. Loop over an array, forwards and backwards
+ * 4. Loop over an object.
+ */

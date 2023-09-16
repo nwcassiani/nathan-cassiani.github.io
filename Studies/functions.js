087323acecbe565098e8c40e8299da253c5540e9
Nathan Cassiani <nwcassiani@gmail.com>
2023-09-16 20:01:46 +0000

    studies functions

--- Studies/functions.js
+++ Studies/functions.js
@@ -2,4 +2,22 @@
  * 
  * FUNCTIONS
  * 
+ * 0. Functions allow us to encapsulate a block of code, pass around this block of code in our program, and execute
+ * this block of code whenever and however many times we want. Functions should take some input and return an output 
+ * where there is some obvious relationship between the input and the output. To use a function, you must define it 
+ * somewhere in the scope from which you wish to call it.
+ * 1. There are two phases to using a function: First, the declaration or defintion, which creates the function, and
+ * second, the calling or invocation of a function, which executes or uses the function.
+ * 2. Functions take inputs, which we call parameters. They can take as many parameters as we want, or they can take
+ * no parameters. Arguments are the values which our functions take in when we invoke functions. So, parameters are
+ * like placeholders for arguments.
+ * 3. The syntax for a named function starts with the keyword function followed by a name for the function, then 
+ * parentheses which contain the parameters, then curly braces which contain the code block.
+ * 4. To assign a function to a variable, called a function expression, we declare a variable with keyword and name 
+ * and assign to it the value of a function defintion. Since the name of the variable is the name of the function,
+ * here the name is not include between the keyword function and parameters in parenthesis.
+ * 5. How do we specify inputs and outputs? both are optional
+ * 6. Functions can see and modify variables in parent or global scope. However outer scoped variables cannot do the 
+ * same to variables in inner scopes.
+ * 7. Closures
  */
